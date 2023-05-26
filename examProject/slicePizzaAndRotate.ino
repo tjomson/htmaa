8984db4a79b7858125ce8f8bf3c64a3ec79e7787
Thomas Hoffmann Kilbak <thki@queue-it.com>
2023-05-26 13:26:24 +0200

    Allow choosing slice count

--- examProject/slicePizzaAndRotate.ino
+++ examProject/slicePizzaAndRotate.ino
@@ -1,10 +1,19 @@
 const int step_pin = 10;
 const int dir_pin = 11;
 
-const int step_pin_base = 7;
-const int dir_pin_base = 6;
+const int step_pin_base = 9; //was 7
+const int dir_pin_base = 8; //was 6
 
-const int sensor_pin = 2;
+const int sensor_pin = 7; // was 2
+
+const int go_button_pin = 12;
+const int increment_button_pin = 13;
+
+const int led_2_pin = 2;
+const int led_4_pin = 3;
+const int led_6_pin = 4;
+const int led_8_pin = 5;
+
 
 const int stepsPerRotation = 200;
 const float degreesPerStep = 1.8;
@@ -13,6 +22,10 @@ const float cmPerRotation = 3.9;
 const int toSensorSide = 0;
 const int toMotorSide = 1;
 
+int cutCountSet = 1;
+
+unsigned long lastReadTime = millis();
+
 void setup()
 {
     pinMode(step_pin, OUTPUT);
@@ -20,6 +33,14 @@ void setup()
     pinMode(step_pin_base, OUTPUT);
     pinMode(dir_pin_base, OUTPUT);
     pinMode(sensor_pin, INPUT);
+
+    pinMode(go_button_pin, INPUT);
+    pinMode(increment_button_pin, INPUT);
+
+    pinMode(led_2_pin, OUTPUT);
+    pinMode(led_4_pin, OUTPUT);
+    pinMode(led_6_pin, OUTPUT);
+    pinMode(led_8_pin, OUTPUT);
     // initialize the serial port:
     Serial.begin(9600);
     cutUntilLimit(toMotorSide); // find home
@@ -73,12 +94,34 @@ void rotateBase(int degrees) {
   }
 }
 
+void setLeds() {
+  digitalWrite(led_2_pin, HIGH);
+  digitalWrite(led_4_pin, cutCountSet >= 2);
+  digitalWrite(led_6_pin, cutCountSet >= 3);
+  digitalWrite(led_8_pin, cutCountSet >= 4);
+}
+
+void readIncrement() {
+  unsigned long now = millis();
+  if (now - lastReadTime > 400 && digitalRead(increment_button_pin)) {
+    cutCountSet++;
+    if (cutCountSet >= 5) cutCountSet = 1;
+    lastReadTime = now;
+  }
+}
+
 void loop()
 {
-    cutUntilLimit(toSensorSide);
-    delay(500);
-    cutUntilLimit(toMotorSide);
-    delay(500);
-    rotateBase(90);
-    delay(500);
+  readIncrement();
+  setLeds();
+  if (digitalRead(go_button_pin)) {
+    for (int i = 0; i < cutCountSet; i++) {
+      cutUntilLimit(toSensorSide);
+      cutUntilLimit(toMotorSide);
+      if (i < cutCountSet - 1) {
+        rotateBase(180 / cutCountSet);
+        delay(500);
+      }
+    }
+  }
 }
